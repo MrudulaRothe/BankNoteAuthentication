13d0ec7b55200317d198adf425015c06338ffdb9
Mrudula <Mrudu@Mrudula.com>
2022-04-05 19:10:48 +0530

    first commit

---
+++ flaskapp.py
@@ -0,0 +1,35 @@
+######## tested with postman ######
+from flask import Flask, request
+import pandas as pd
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+pickle_in = open('classifier.pkl', 'rb')  # read-byte mode
+classifier = pickle.load(pickle_in)
+
+
+@app.route('/')  # it is a decorator and by default it is a GET method
+def welcome():
+    return "Welcome All"
+
+
+@app.route('/predict')
+def predict_note_authentication():
+    variance = request.args.get('variance')
+    skewness = request.args.get('skewness')
+    curtosis = request.args.get('curtosis')
+    entropy = request.args.get('entropy')
+    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
+    return prediction
+
+
+@app.route('/predict_file', methods=["POST"])
+def predict_note_file():
+    df_test = pd.read_csv(request.files.get("file"))
+    prediction = classifier.predict(df_test)
+    return prediction
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import pandas as pd
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+pickle_in = open('classifier.pkl', 'rb')  # read-byte mode
+classifier = pickle.load(pickle_in)
+
+
+@app.route('/') # it is a decorator and by default it is a GET method
+def welcome():
+    return "Welcome All"
+
+@app.route('/predict')
+def predict_note_authentication():
+    variance = request.args.get('variance') 
+    skewness = request.args.get('skewness')
+    curtosis = request.args.get('curtosis')
+    entropy = request.args.get('entropy')
+    prediction = classifier.predict([[variance,skewness,curtosis,entropy]])
+    return "The predicted value is"+str(prediction)
+
+if __name__ == '__main__':
+    app.run()
